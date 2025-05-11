0073f0d42f4d8d5b2216addba12c8604e195c8b0
Abdul Rahim <chaudharyabdulrahimamir@gmail.com>
2025-05-11 22:15:50 +0500

    changes in App.jsx

--- frontend/gifme/src/App.jsx
+++ frontend/gifme/src/App.jsx
@@ -1,16 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import UploadGIFPage from './pages/UploadGIFPage'; // Home page
+import LoginPage from "./pages/LoginPage";
+import SignupPage from "./pages/SignupPage";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-    <h1 className='text-6xl text-red-700 bg-amber-500'>hey</h1>
-    </>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<UploadGIFPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignupPage />} />
+        <Route path="*" element={<LoginPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
